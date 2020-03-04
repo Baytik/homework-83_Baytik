58696430741d79d76748a3521f9fd2f8b36dd50f
Baytik Tumanbaev <jumka.wows@gmail.com>
2020-03-04 22:39:11 +0600

    Добавил bcrypt

--- app/users.js
+++ app/users.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const User = require('../models/User');
+const bcrypt = require("bcrypt");
 
 const router = express.Router();
 
@@ -20,6 +21,19 @@ router.post('/', async (req, res) => {
    } catch (error) {
        return res.send(400).send(error);
    }
+});
+
+router.post('/sessions', async (req, res) => {
+   const user = await User.findOne({username: req.body.username});
+
+   if (!user) {
+       return res.status(400).send({error: 'Username or password not correct!'})
+   }
+   const isMatch = await bcrypt.compare(req.body.password, user.password);
+   if (!isMatch) {
+       return res.status(400).send({error: 'Username or password not correct!'})
+   }
+   return res.send({message: 'Username and password correct'})
 });
 
 module.exports = router;
